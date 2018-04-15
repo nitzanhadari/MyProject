0cc1894f92e5d67c11ed897dd5fac541c75dbfac
EC2 Default User <ec2-user@ip-172-31-16-24.us-east-2.compute.internal>
2018-04-15 15:54:01 +0000

    get ip function +api

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,6 +1,11 @@
 #!flask/bin/python
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(sys.path[0])))
+
+import requests
+
 import json
-from flask import Flask, Response
+from flask import Flask, Response, request
 from helloworld.flaskrun import flaskrun
 
 application = Flask(__name__)
@@ -8,10 +13,26 @@ application = Flask(__name__)
 @application.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    
+    
+    
+@application.route('/get_ip', methods=['GET'])
+def get_ip():
+   # print(get_ip_meta())
+    return Response(json.dumps(get_ip_meta()), mimetype='application/json', status=200)
+
+
 
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    
+def get_ip_meta():
+    user_ip = str(request.environ['REMOTE_ADDR'])
+    service_url = 'http://ipinfo.io/{}'.format(user_ip) 
+    return requests.get(service_url).json()
 
 if __name__ == '__main__':
     flaskrun(application)
+    
+
