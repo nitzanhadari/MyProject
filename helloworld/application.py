1dfe9e4ce55a0d3e570e2533b785e6edbf9e2e86
EC2 Default User <ec2-user@ip-172-31-16-24.us-east-2.compute.internal>
2018-05-27 15:42:36 +0000

    first commit

--- helloworld/application.py
+++ helloworld/application.py
@@ -7,6 +7,9 @@ import requests
 import json
 from flask import Flask, Response, request
 from helloworld.flaskrun import flaskrun
+import boto3
+import datetime
+
 
 application = Flask(__name__)
 
@@ -31,8 +34,32 @@ def get_ip_meta():
     user_ip = str(request.environ['REMOTE_ADDR'])
     service_url = 'http://ipinfo.io/{}'.format(user_ip) 
     return requests.get(service_url).json()
+    
+@application.route('/temp/<temp>', methods=['POST'])
+def get_temp(temp):
+    # get ip metadata from the fuction
+    response = get_ip_meta()
+    # create a session for boto to access the credentials that the ec2 holds
+    my_ses = boto3.Session(region_name = 'us-east-2')
+    # connect to the resource dynmodb using the session
+    dynamodb = my_ses.resource('dynamodb')
+    # refer to the table
+    table = dynamodb.Table('first_table')
+
+    item={
+    'ip_address': str(response), 
+    'path': temp,
+    'datetime': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    'time': datetime.datetime.now().strftime("%H:%M:%S"),
+    'ip_meta' : response, # res_data
+    'name':'chilkibilki'
+    }
+    
+    print(item)
+    # insert the item
+    table.put_item(Item=item)
+    
+    return Response(json.dumps(item), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
-    
-
